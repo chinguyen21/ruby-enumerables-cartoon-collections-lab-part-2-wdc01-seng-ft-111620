1a9cf0dd230c8066fbf040fd4b5bfd6228f22a36
Chi Nguyen <nguyen.thimai@northeastern.edu>
2020-10-24 20:45:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,12 +7,16 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(planeteer_calls)
-  # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
-  # Return the boolean result of this check
+  planeteer_calls.all? {|element| element.length > 4}
+  
 end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
-  # Return the first valid call found, or return nil if no valid calls are found
+  planeteer_calls.each do |element|
+    if valid_calls.include?(element)
+      return element
+    end
+  end
+  return nil
 end
